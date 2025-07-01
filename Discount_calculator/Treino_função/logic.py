2ffce1bd482480dbc7360d87261ec07bf9f11fa8
Smithandersonn <dexterplay135@gmail.com>
2025-06-30 21:54:35 -0300

    Update logic.py

--- Discount_calculator/Treino_função/logic.py
+++ Discount_calculator/Treino_função/logic.py
@@ -11,5 +11,13 @@ def cal_desconto (salario, percentual_de_desconto):
 
 #Chamando Função 
 resultado = cal_desconto(salario, percentual_de_desconto)
-#percentual_de_desconto:g (:g) remove (.0) se o número for inteiro (ex: 5.0 → 5)
+
+"""
+percentual_de_desconto:g (:g) remove (.0) se o número for inteiro (ex: 5.0 → 5)
+o f no inicio da variavel permite inserir variáveis diretamente dentro da string usando chaves {}
+:.2f (Formatação de números) 
+: → Indica o início da formatação.
+.2 → Define 2 casas decimais após o ponto.
+f → Indica que é um float (número decimal).
+"""
 print (f"O Salario com Desconto de {percentual_de_desconto:g}% é:R${resultado :.2f}")
